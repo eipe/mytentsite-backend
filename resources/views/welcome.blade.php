308a9046f1eca6741b57e75d05af8ba965ce032b
Eivind Røe <eivindroe@gmail.com>
2016-11-07 22:50:07 +0100

    Added frontend of application
    This includes all parts of the mytentsite-frontend repo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,88 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <title>Mytentsite - a collaborative tent collective</title>
+        <link href="{{ asset('/css/vendor.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/app_normalize.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+        <nav class="top-bar">
+            <ul id="menu" class="menu align-center" data-page-default="map">
+                <li data-page="photo">
+                    <a href="#/photo"><i class="fa fa-camera"></i></a>
+                </li>
+                <li data-page="map">
+                    <a href="#/map" title="">
+                        <i class="fa fa-map-o"></i>
+                    </a>
+                </li>
+                <li data-page="wall">
+                    <a href="#/wall" title="">
+                        <i class="fa fa-th"></i>
+                    </a>
+                </li>
+                <li data-page="info">
+                    <a href="#/info" title="Information"><i class="fa fa-info"></i></a>
+                </li>
+            </ul>
+        </nav>
+        <div id="content">
+            <div class="page is-hidden" id="photo">
+                <div id="photo-frame">
+                    <label for="photo-file" class="button" data-text="I have a stored photo to upload!">
+                        I have a stored photo to upload!
+                    </label>
+                    <input type="file" id="photo-file" class="show-for-sr">
+                </div>
+                <div id="photo-controllers" class="input-group is-hidden">
+                    <span class="input-group-label" title="Caption"><i class="fa fa-font"></i></span>
+                    <input type="text" id="photo-caption" class="input-group-field"
+                           title="Caption" placeholder="Caption" />
+                    <div class="input-group-button">
+                        <button id="photo-location" class="button secondary" title="Add location">
+                            <i class="fa fa-map-marker"></i>
+                        </button><button id="photo-cancel" class="button alert" title="Cancel">
+                            <i class="fa fa-remove"></i>
+                        </button><button id="photo-store" class="button success" title="Upload photo">
+                            <i class="fa fa-check"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="page is-hidden" id="map"></div>
+            <div class="page is-hidden" id="wall">
+                <div id="wall-fullscreen" class="wall-image-container reveal" data-reveal data-close-on-click="true">
+                    <div class="wall-image-controllers">
+                        <i class="wall-image-view-map wall-image-enlarged fa fa-map-marker"
+                           title="View image on map"></i>
+                        <i class="wall-image-close fa fa-times" data-close title="Close"></i>
+                    </div>
+                    <img src="">
+                </div>
+                <div id="wall-images">
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                    <div class="wall-image-container"></div>
+                </div>
+                <br />
+                <div class="row">
+                    <button class="button float-center is-hidden" id="wall-load-more">Load more images</button>
+                </div>
+                <div class="row"><br /><br /></div>
+            </div>
+            <div class="page is-hidden" id="info">
+                <h1>About mytent.site</h1>
             </div>
         </div>
+        <script src="{{ asset('/js/vendor.js') }}"></script>
+        <script src="{{ asset('/js/app.js') }}"></script>
     </body>
 </html>

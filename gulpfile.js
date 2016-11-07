ab0e6f20b0c281edda82a940cb375ef110fc2b76
Eivind Røe <eivindroe@gmail.com>
2016-11-07 22:48:31 +0100

    Updated gulp file with new frontend dependencies
    This compiles all application and vendor assets into a few specific files used in view blades

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
-var elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir'),
+    gulp = require('gulp');
+
+var config = {
+    bowerDir: '../../../vendor/bower_components'
+};
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +17,35 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    //mix.sass('app.scss', 'public/css/app.css');
+
+    mix.styles('app.css', 'public/css/app.css')
+        .styles('app_normalize.css', 'public/css/app_normalize.css')
+        .styles([
+        config.bowerDir + '/leaflet/dist/leaflet.css',
+        config.bowerDir + '/leaflet.markercluster/dist/MarkerCluster.css',
+        config.bowerDir + '/leaflet.markercluster/dist/MarkerCluster.Default.css',
+        config.bowerDir + '/Leaflet.Photo/Leaflet.Photo.css',
+        config.bowerDir + '/Leaflet.EasyButton/src/easy-button.css',
+        config.bowerDir + '/font-awesome/css/font-awesome.css',
+        config.bowerDir + '/foundation-sites/dist/foundation-flex.css',
+        config.bowerDir + '/foundation-sites/dist/foundation.css'
+    ], 'public/css/vendor.css');
+
+    mix.scripts('app.js', 'public/js/app.js')
+        .scripts([
+        config.bowerDir + '/jquery/dist/jquery.js',
+        config.bowerDir + '/leaflet/dist/leaflet.js',
+        config.bowerDir + '/leaflet.markercluster/dist/leaflet.markercluster.js',
+        config.bowerDir + '/Leaflet.Photo/Leaflet.Photo.js',
+        config.bowerDir + '/Leaflet.EasyButton/src/easy-button.js',
+        config.bowerDir + '/foundation-sites/dist/foundation.js',
+        config.bowerDir + '/exif-js/exif.js',
+        config.bowerDir + '/what-input/what-input.js'
+    ], 'public/js/vendor.js');
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('vendor/bower_components/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('public/fonts'));
 });
